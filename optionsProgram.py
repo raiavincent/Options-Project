e628e858d02cf5600bc45cb427832ab3f9311a29
raiavincent <61218788+raiavincent@users.noreply.github.com>
2020-05-08 18:35:51 -0400

    adding in the necessary inputs for v1

--- optionsProgram.py
+++ optionsProgram.py
@@ -5,3 +5,22 @@ import pandas
 import matplotlib
 import numpy
 import yfinance
+import bs4 # for the web scraping on yahoo finance
+import pprint
+import pyinputplus as pyip
+
+# Start by getting the current price of the option.
+
+optionPrice = pyip.inputNum("Enter the current price of the option contract: ")
+
+# Next we will get the price of the stock.
+
+stockPrice = pyip.inputNum("Enter the current price of the stock: ")
+
+# Maybe I also need the strike price of the option
+
+strikePrice = pyip.inputNum("Enter the strike price associated with the contract: ")
+
+# Probably also need the premium!
+
+premium = pyip.inputNum("Enter the premium associated with the option: ")
