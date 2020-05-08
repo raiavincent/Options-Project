e73531750b845708b9976d001b3c30e27c53eb69
raiavincent <61218788+raiavincent@users.noreply.github.com>
2020-05-08 18:26:11 -0400

    initializing

---
+++ optionsProgram.py
@@ -0,0 +1,7 @@
+# start of the build out of the options program
+# starting super simple and calculating the current profitability of the option
+
+import pandas
+import matplotlib
+import numpy
+import yfinance
