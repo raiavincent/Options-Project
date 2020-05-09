14a21b299731909b64234327e06d418a98351875
raiavincent <61218788+raiavincent@users.noreply.github.com>
2020-05-09 02:03:18 -0400

    Covered Call
    added support for calculation of a covered call.

--- optionsProgram.py
+++ optionsProgram.py
@@ -2,6 +2,10 @@
 # starting super simple and calculating the current
 # profitability of the option before any web scraping or anything like that
 
+# UP TOP TO DO:
+# Round all outputs to two decimal places.
+
+
 import pandas
 import matplotlib
 import numpy
@@ -12,7 +16,7 @@ import pyinputplus as pyip
 import os
 import sys
 
-# Calculating call option
+# Calculations on call option,
 
 # Start defining the calculations.
 
@@ -39,6 +43,8 @@ def callCalc():
 
     print("The current time value is " + str(timeValue))
 
+# Calculations on put option.
+
 def putCalc():
     optionPrice = pyip.inputNum("Enter the current price of the option contract: ")
 
@@ -62,7 +68,29 @@ def putCalc():
 
     print("The current time value is " + str(timeValue))
 
-option = pyip.inputMenu(['call','put','q'], numbered = True)
+# Calculating a covered call, which is where an investor may like the stock, but to cover some downside risk in the mean
+# time, will sell a call option to generate income on the premiums.
+
+def coveredCall():
+
+    # Stock price first, this is what the investor bought the stock at.
+
+    purchasePrice = pyip.inputNum("Enter the price of the stock the investor paid: ")
+
+    # Then we need the premium received for selling the call.
+
+    premium = pyip.inputNum("Enter the premium the investor gained by selling the call: ")
+
+    # Current price of the stock to determine the investor's position.
+
+    stockPrice = pyip.inputNum("Enter the current price of the stock to determine the investor's yield: ")
+
+    # And determin the yield currently as of now.
+
+    coveredYield = premium + stockPrice - purchasePrice
+    print('The investors current yield is ' + str(coveredYield))
+
+option = pyip.inputMenu(['call','put','covered call','q'], numbered = True)
 
 if option == 'call':
     print('Starting call option calculation.')
@@ -70,6 +98,9 @@ if option == 'call':
 elif option == 'put':
     print('Starting put option calculation.')
     putCalc()
+elif option == 'covered call':
+    print('Starting the calculation of a covered call.')
+    coveredCall()
 elif option == 'q':
     print('Stopping execution of the program.')
     sys.exit()
